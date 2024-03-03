a377dab7ba1c7ab3efec2fab0d9cca6ac3b72303
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-03-03 14:42:32 +0300

    feat: added builder for user 2

---
+++ src/main/java/Entity/Users/UserBuilder.java
@@ -0,0 +1,37 @@
+package Entity.Users;
+
+import Tools.InvalidValueException;
+
+public class UserBuilder {
+    private String myFirstName;
+    private String mySecondName;
+    private String myAddress;
+    private int myPassport;
+
+    public UserBuilder setFirstName(String firstName) throws InvalidValueException {
+        if (firstName.isEmpty()) throw new InvalidValueException();
+        myFirstName = firstName;
+        return this;
+    }
+
+    public UserBuilder setSecondName(String secondName) throws InvalidValueException {
+        if (secondName.isEmpty()) throw new InvalidValueException();
+        mySecondName = secondName;
+        return this;
+    }
+
+    public UserBuilder setAddress(String address) throws InvalidValueException {
+        if (address.isEmpty()) throw new InvalidValueException();
+        myAddress = address;
+        return this;
+    }
+
+    public UserBuilder setPassport(int passport) throws InvalidValueException {
+        if (passport < 0) throw new InvalidValueException();
+        myPassport = passport;
+        return this;
+    }
+    public User create() {
+        return new User(myFirstName, mySecondName, myAddress, myPassport);
+    }
+}

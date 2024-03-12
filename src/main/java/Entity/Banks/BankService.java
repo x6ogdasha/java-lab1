c01a22d20e66c49e2b3672fb2baf42f01cf34355
Bogdan Kitaev <6ogdashaa@gmail.com>
2024-03-12 11:47:36 +0300

    refactor: minor changes

--- src/main/java/Entity/Banks/BankService.java
+++ src/main/java/Entity/Banks/BankService.java
@@ -1,6 +1,18 @@
 package Entity.Banks;
 
+import Entity.Users.User;
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+@RequiredArgsConstructor
 public class BankService {
+
+    @NonNull private final ArrayList<Bank> banks;
+    @NonNull Map<User, ArrayList<Integer>> users;
 
     private final Integer emperorPassword = 1902;
     Boolean isEmperorLogged = false;
@@ -11,4 +23,9 @@ public class BankService {
         }
 
     }
+
+    public Boolean checkBankLogin(String bankName, Integer password) {
+        return banks.stream().anyMatch(x -> x.getName() == bankName && x.getPin() == password);
+    }
+
 }
